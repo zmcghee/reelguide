317f6bd46f74026e8a9f449ba165fc249abb6526
Zack McGhee <zack.mcghee@gmail.com>
2015-05-21 17:07:42 -0500

    Add docstrings to GoogleSheet class

--- repertory/utils/spreadsheet.py
+++ repertory/utils/spreadsheet.py
@@ -1,13 +1,42 @@
 import json
 import requests
 
-from repertory.utils.calendar import CalendarImport
-
 class GoogleSheet(object):
+    """
+    Class to import the first sheet of a Google Sheet and
+    convert it to a Python dictionary. Basic usage:
+    
+        >>> sheet_id = '1PWhG8sLvoGM86OTBPIc7lrWYnqoAQPo9hzrEJRnKogE'
+        >>> sheet = GoogleSheet(sheet_id)
+        >>> sheet.items
+    """
     sheet_id = None
     fields = []
 
     def __init__(self, sheet_id, fields=[], fetch=True):
+        """Class will fetch the remote data and introspect fields
+        from the data by default upon instantiation. You can pass
+        fetch=False to disable this behavior and call them
+        explicitly, such as:
+
+            >>> sheet = GoogleSheet(sheet_id, fetch=False)
+            >>> sheet.fetch()
+            >>> sheet.auto_fields()
+            >>> sheet.items
+            [(list of dicts)...]
+
+        You can also explicitly pass the fields you want returned,
+        either here or using the set_fields method. Examples:
+        
+            >>> sheet = GoogleSheet(sheet_id, fields=['name', 'email'])
+            >>> sheet.items.keys()
+            ['name', 'email']
+
+            >>> sheet = GoogleSheet(sheet_id, fetch=False)
+            >>> sheet.set_fields(['name', 'email'])
+            >>> sheet.items.keys()
+            ['name', 'email']
+        """
         self.sheet_id = sheet_id
         if fields: # before fetch in case fetch fails
             self.set_fields(fields)
@@ -19,23 +48,28 @@ class GoogleSheet(object):
 
     @property
     def json_url(self):
+        """Returns a simple URL to fetch the first sheet of
+        a Google Sheet in JSON format from Google"""
         url = 'https://spreadsheets.google.com/feeds/list/'\
             + '%s/default/public/values?alt=json' % (self.sheet_id)
         return url
 
     def fetch(self):
+        """Fetch json_url, convert the JSON to Python & store it"""
         self._google = requests.get(self.json_url)
         if self._google.status_code != 200:
             raise Exception("Google Sheet returned non-200 response")
         self.gsx = self._google.json()
 
     def set_fields(self, fields):
+        """Explicitly set fields you want from the sheet"""
         self.fields = fields
 
     def auto_fields(self):
+        """Infer fields from keys in your sheet's first entry"""
         try:
             keys = self.gsx['feed']['entry'][0].keys()
-        except IndexError:
+        except KeyError, IndexError:
             pass
         else:
             fields = []
@@ -46,6 +80,7 @@ class GoogleSheet(object):
 
     @property
     def items(self):
+        """Returns entries from your sheet"""
         items = []
         for entry in self.gsx['feed']['entry']:
             item = {}
