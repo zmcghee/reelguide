398c0f818456db23c21ecf8b8d2fd483bc4f84c6
Zack McGhee <zack.mcghee@gmail.com>
2015-05-22 01:57:37 -0500

    Make it possible to sort calendar feed by title

--- repertory/api/calendar.py
+++ repertory/api/calendar.py
@@ -11,6 +11,11 @@ def upcoming_events(request):
     items = []
     filter = {'datetime__gte': datetime.now()}
     qs = EventInstance.objects.filter(**filter)
+    sort = request.GET.get('sort', '')
+    if sort == 'title':
+        qs = qs.order_by('event__sort_title', 'datetime')
+    else:
+        qs = qs.order_by('datetime')
     for event in qs:
         items.append(event.as_dict)
     return JsonResponse(items, safe=False)
